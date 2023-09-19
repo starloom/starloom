3dbe199ae90332b0fc2652626c356ed438fdebb4
Starloom <141710037+starloom@users.noreply.github.com>
2023-09-18 18:33:59 -0700

    Delete test directory

--- test/mysql_test01.py
+++
@@ -1,8 +0,0 @@
-from sql.execute_sql import _select_xingzuo_question_by_id,_select_xingzuo_ranking_by_question_id
-
-def _test01 () :
-    print('---------')
-    _questions = _select_xingzuo_question_by_id(1)
-    _votes = _select_xingzuo_ranking_by_question_id(1)
-
-_test01()
